48943544b62a42cb1183efd2ead748b72fd89b81
Leonardo Nhemetz <leonardo_nhemetz@hotmail.com>
2014-09-10 16:14:11 -0300

    Ideal Weight
    the software calculates your ideal weight

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Peso_ideal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Sexo;
+            double Altura;
+
+            Console.Write("Escreva seu sexo: ");
+            Sexo = Console.ReadLine();
+            Console.Write("Escreva sua altura: ");
+            Altura = Convert.ToSingle (Console.ReadLine());
+
+
+            if (Sexo == "masculino")
+            {
+                Altura = 72.7 * Altura - 58;
+                
+            }
+            else
+            {
+                Altura = 62.1 * Altura -44.7;
+                
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
